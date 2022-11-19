819a7cba0e47fb91bdfd1fcc7a1a188b26e0b9e6
jaczhi <54284925+jaczhi@users.noreply.github.com>
2022-11-19 15:31:54 -0800

    Collaboratively implement

--- hells-kitchen/getUniqueIngredients.h
+++ hells-kitchen/getUniqueIngredients.h
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <string>
+#include <unordered_set>
+
 using namespace std;
 
 int getUniqueIngredients(string ingredients[], int size) {
-    return 0;
+    unordered_set <string> myset;
+    for (int i = 0; i < size; i++) {
+        string clean = "";
+        for (int j = 0; j < ingredients[i].size(); j++) {
+            char curr = ingredients[i][j];
+            if (curr >= 'A' && curr <= 'Z') {
+                curr += 32;
+            }
+            
+            // after this point curr is correctly lowercase
+            //  and the whole string is correct after this point 
+            
+            if (curr >= 'a' && curr <= 'z') {
+                clean += curr;
+            }
+        }
+        myset.insert(clean);
+    }
+    return myset.size();
 }
--- hells-kitchen/testGetUniqueIngredients.cpp
+++ hells-kitchen/testGetUniqueIngredients.cpp
@@ -5,8 +5,26 @@ using namespace std;
 int main() {
     // add your own test cases here!
     string ingredients1[] = {"egg", "tomato", "cheese", "bread", "jam"};
-    if(getUniqueIngredients(ingredients1, 5) == 5){
-        cout << "Passed test case!" << endl;
+    string ingredients2[] = {"eggs", "egg54s", "s0ou#@p", "\\,89s"};
+    string ingredients3[] = {"ch.ick.en wi.n.g.s", "onion@rings000", "chickenwings", "oNioNriNgs"};
+    string ingredients4[] = {"cheese", "cheEse", "CHEESE", "cheese0", "\\_cheese\\_", "c-h-e-e-s-e", "c0h0e0e0s0e", "c      heese0"};
+    if(getUniqueIngredients(ingredients1, 5) == 5) {
+        cout << "Passed test case! 1" << endl;
+    } else {
+        cout << "Failed test case :(" << endl;
+    }
+    if(getUniqueIngredients(ingredients2, 4) == 3){
+        cout << "Passed test case! 2" << endl;
+    } else {
+        cout << "Failed test case :(" << endl;
+    }
+    if(getUniqueIngredients(ingredients3, 4) == 2){
+        cout << "Passed test case! 3" << endl;
+    } else {
+        cout << "Failed test case :(" << endl;
+    }
+    if(getUniqueIngredients(ingredients4, 8) == 1){
+        cout << "Passed test case! 4" << endl;
     } else {
         cout << "Failed test case :(" << endl;
     }
